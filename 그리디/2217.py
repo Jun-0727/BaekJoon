b307dfa4095dde53d7177a22881947f2f50b8aa1
jetty <dlwnsdud980727@gmail.com>
2023-07-14 10:57:20 +0900

    Greedy - 로프

---
+++ 그리디/2217.py
@@ -0,0 +1,42 @@
+# 가장 굵은 로프를 찾는다
+# 가장 얇은 로프 * 로프의 갯수 구하고, 가장 얇은거 pop
+# 끝까지 반복
+"""
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+ropes = [int(input()) for _ in range(N)]
+
+candidates = [0]
+
+for i in range(len(ropes)):
+    weight = min(ropes) * len(ropes)
+    
+    if (weight > max(candidates)):
+        candidates.append(weight)
+    
+    ropes.pop(ropes.index(min(ropes)))
+
+print(max(candidates))
+"""
+# >> 시간 초과
+
+# ropes를 sorting해서 max(roeps)가 아니라 위에서 부터 하나씩 꺼내자
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+ropes = [int(input()) for _ in range(N)]
+ropes = sorted(ropes)
+
+candidates = [0]
+
+for i in range(N):
+    weight = ropes[i] * (N - i)
+    
+    if (weight > max(candidates)):
+        candidates.append(weight)
+
+print(max(candidates))
+
